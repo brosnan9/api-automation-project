f57ee8a0fad301fb5aef79c9975f91dc144cf3b9
Gerson Brosnan Pereira <gersonbrosnanpereira@Gersons-MacBook-Pro.local>
2023-10-28 23:34:07 +0700

    Adding serialisation concept and spec builder to create Location

---
+++ src/test/java/courses/coursesCalculate.java
@@ -0,0 +1,2 @@
+package courses;public class coursesCalculate {
+}
---
+++ src/test/java/googleLocation/serializeLocation.java
@@ -0,0 +1,46 @@
+package googleLocation;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.path.json.JsonPath;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.Test;
+import pojo.*;
+
+import java.util.ArrayList;
+
+import static io.restassured.RestAssured.given;
+public class serializeLocation {
+    static RequestSpecification req = new RequestSpecBuilder().addQueryParam("key", "qaclick123").build();
+    static ResponseSpecification res = new ResponseSpecBuilder().expectStatusCode(200).build();
+    @Test
+    public void buildLocationPayload(){
+
+        googleLocationPayload gl = new googleLocationPayload();
+        gl.setAccuracy(50);
+        gl.setName("Siaga Raya");
+        gl.setPhone_number("+91976927");
+        gl.setAddress("3J, Side Layout");
+        gl.setWebsite("www.google.com");
+        gl.setLanguage("English");
+
+        location l = new location();
+        l.setLat(-38.383494);
+        l.setLng(33.427362);
+        gl.setLocation(l);
+
+        ArrayList list = new ArrayList<>();
+        list.add("Shoe Shop");
+        list.add("Park");
+        gl.setTypes(list);
+
+        String endpoint = "https://rahulshettyacademy.com/maps/api/place/add/json";
+        var createResponse = given().spec(req).body(gl).
+                when().post(endpoint).
+                then().spec(res).extract().response().asString();
+        System.out.println(createResponse);
+
+
+    }
+}
